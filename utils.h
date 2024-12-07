3ca015729e0dece6d5cd0658a86e70b53ec6c84e
Lucas Cabral <122704555+lucascamourao@users.noreply.github.com>
2024-12-07 14:51:39 -0300

    Create utils.h

---
+++ utils.h
@@ -0,0 +1,16 @@
+#ifndef UTIL_H
+// verifica se não está definida
+
+#define UTIL_H
+// define a macro UTIL_H
+
+#include <stdbool.h>
+
+// Macro útil
+#define MAX_BUFFER_SIZE 1024
+
+// Funções utilitárias
+bool is_empty(char *fila);
+bool is_prime(int number);
+
+#endif // fim da verificação
