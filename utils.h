6c0bac7484e6f103d011cfc7d1767a0f19a3be7e
Lucas Cabral <122704555+lucascamourao@users.noreply.github.com>
2024-12-07 14:56:12 -0300

    Update utils.h

--- utils.h
+++ utils.h
@@ -5,12 +5,22 @@
 // define a macro UTIL_H
 
 #include <stdbool.h>
+#include <sys/types.h>
 
 // Macro útil
 #define MAX_BUFFER_SIZE 1024
 
+// Estrutura do cliente
+typedef struct {
+    pid_t pid;             // Identificador do processo
+    int hora_chegada;      // Hora de chegada em milissegundos
+    int prioridade;        // 0: baixa, 1: alta
+    int tempo_atendimento; // Tempo necessário para atendimento
+} Cliente;
+
 // Funções utilitárias
 bool is_empty(char *fila);
 bool is_prime(int number);
+void exibir_cliente(const Cliente *cliente);
 
 #endif // fim da verificação
